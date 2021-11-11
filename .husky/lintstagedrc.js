668dcca2f992bdcaeb5d509669a486df60a409bb
xiaoxian521 <1923740402@qq.com>
2021-11-11 09:47:28 +0800

    docs: update

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": [
+  "{!(package)*.json,./vscode/*.code-snippets,.!(browserslist)*rc}": [
     "prettier --write--parser json"
   ],
   "package.json": ["prettier --write"],
