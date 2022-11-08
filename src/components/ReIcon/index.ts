e183ea75a0058182f7bd92eb2de74287989fc40f
xiaoxian521 <1923740402@qq.com>
2022-11-08 12:10:54 +0800

    feat: 菜单图标 `icon` 支持使用在线图标

--- src/components/ReIcon/index.ts
+++ src/components/ReIcon/index.ts
@@ -2,7 +2,7 @@ import iconifyIconOffline from "./src/iconifyIconOffline";
 import iconifyIconOnline from "./src/iconifyIconOnline";
 import fontIcon from "./src/iconfont";
 
-/** 离线图标组件 */
+/** 本地图标组件 */
 const IconifyIconOffline = iconifyIconOffline;
 /** 在线图标组件 */
 const IconifyIconOnline = iconifyIconOnline;
