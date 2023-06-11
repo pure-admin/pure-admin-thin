10e45989793b6da62158f7852cd27cc5315e5752
valarchie <343928303@qq.com>
2023-06-11 15:55:27 +0800

    chore: sync with upstream

--- build/info.ts
+++ build/info.ts
@@ -21,8 +21,8 @@ export function viteBuildInfo(): Plugin {
         bold(
           green(
             `👏欢迎使用${blue(
-              "[vue-pure-admin]"
-            )}，如果您感觉不错，记得点击后面链接给个star哦💖 https://github.com/pure-admin/vue-pure-admin`
+              "[Agileboot全栈项目]"
+            )}，如果您感觉不错，记得点击后面链接给个star哦💖 https://github.com/valarchie/agileboot-back-end`
           )
         )
       );
