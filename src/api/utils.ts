ca89c4c501a27ac344ed1dcf3969963837266c95
pan <13329870472@163.com>
2024-03-13 17:59:07 +0800

    perf: 优化

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,3 +1,39 @@
+import { http } from "@/utils/http";
+import type { ApiAbstract } from "@/utils/http/ApiAbstract";
+
 export const baseUrlApi = (url: string) => `/api/${url}`;
 export const baseUrlAuth = (url: string) => `/auth/${url}`;
 export const baseUrlAvatar = (url: string) => `/avatar/${url}`;
+
+export const get = (params: any) => {
+  return http.request<ApiAbstract>("get", baseUrlApi("menus"), {
+    params
+  });
+};
+
+export const add = (data: any) => {
+  return http.request("post", baseUrlApi("menus"), {
+    data
+  });
+};
+
+export const del = (ids: number[] | any) => {
+  return http.request("delete", baseUrlApi("menus"), {
+    data: ids
+  });
+};
+export const edit = (data: any) => {
+  return http.request<ApiAbstract>("put", baseUrlApi("menus"), {
+    data
+  });
+};
+export const download = (data: any) => {
+  return http.request<Blob>(
+    "get",
+    baseUrlApi("menus/download"),
+    {
+      data
+    },
+    { responseType: "blob" }
+  );
+};
