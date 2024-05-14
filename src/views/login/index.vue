df842d93e6e40d1075dd88b62969778963d4975d
xiaoxian521 <1923740402@qq.com>
2024-05-14 12:21:31 +0800

    release: update `5.6.0`

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -64,8 +64,6 @@ const onLogin = async (formEl: FormInstance | undefined) => {
           }
         })
         .finally(() => (loading.value = false));
-    } else {
-      return fields;
     }
   });
 };
